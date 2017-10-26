14d396542f2bb26a6dff1d624e8c0cf07b09bdf7
Alex Berry <berry3@indiana.edu>
2017-10-26 17:46:22 -0400

    initial commit

---
+++ src/main/kotlin/org/almibe/weaseltemplate/WeaselTemplateEngine.kt
@@ -0,0 +1,13 @@
+package org.almibe.weaseltemplate
+
+import com.google.gson.JsonObject
+import java.io.File
+
+class WeaselTemplateEngine(val classLoader: ClassLoader) {
+    fun processTemplate(templateName: String, data: JsonObject): String {
+        val templateUrl = classLoader.getResource(templateName)
+        val templateFile = File(templateUrl.toURI())
+
+        return ""
+    }
+}
