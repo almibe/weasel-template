9b7a1a833c1430718f59aa54edb49efd724a93bf
Alex Berry <berry3@indiana.edu>
2017-10-27 00:09:17 -0400

    updating names + adding empty test files

--- src/main/kotlin/org/almibe/weaseltemplate/WeaselTemplateEngine.kt
+++ src/main/kotlin/org/almibe/weaseltemplate/WeaselTemplateEngine.kt
@@ -3,7 +3,7 @@ package org.almibe.weaseltemplate
 import com.google.gson.JsonObject
 import java.io.File
 
-class WeaselTemplateEngine(val classLoader: ClassLoader) {
+class WeaselTemplateEngine(private val classLoader: ClassLoader) {
     fun processTemplate(templateName: String, data: JsonObject): String {
         val templateUrl = classLoader.getResource(templateName)
         val templateFile = File(templateUrl.toURI())
