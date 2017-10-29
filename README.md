57a923e41f1ef5aa4c0a1ade734c8c4617cf10e8
Alex Berry <berry3@indiana.edu>
2017-10-28 20:37:04 -0400

    committing more blank methods + updating README, #2

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# weasel-template
+Weasel Template
+===============
+
+*This project is still under development.*
+
 A minimalist templating engine for the JVM based on JSON &amp; implemented in Kotlin.
+
+## Features
+- Use JSON for datamodel (via GSON)
+- Target HTML output
+- Play well with OSGi
+- Support a minimal number of features
+
+## Commands
+- `<$data.name>` - inject the value of name that was passed to the template
+- `<$if data.object>` - only include the following template if object exists OR object is true if object is a boolean (the type is checked so the string "false" is considered to be true)
+- `<$each data.list as item>` - loop over the following template for each item in a list do nothing if data.list doesn't exist (throws exception if parameter isn't a list or if name given isn't unique in scope)
+- `<$each data.map as key value>` - loop over following template for each key value pair in a map do nothing if data.map doesn't exist (throws exception if parameter isn't a map or if names given aren't unique in scope)
+- `<$include 'templateName' arg>` - include the following template and pass arg as the data model (arg is optional, if it isn't present then data is an empty object)
+
+## Syntax
+
+Template files are regular text files with Weasel Template markup wrapped in `<$>`'s.
+
+```html
+<$include 'header.wt' data>
+
+<$if data.isAdmin>
+  <$include 'admin.wt' data>
+<$elseif data.isRegularUser>
+  Hello <$data.name>
+<$else>
+  <$include 'login.wt' data>
+<$end if>
+
+<ul>
+<$each data.insults as insult>
+  <li><$data.name> you are <$insult></li>
+<$end each>
+</ul>
+```
+
+## Kotlin API
+
+```kotlin
+val engine = WeaselTemplateEngine(this.class) //you can/probably should reuse this
+val data = //create JSON object
+val result = engine.process('templatename.wt', data)
+```
--- src/main/kotlin/org/almibe/weaseltemplate/WeaselTemplateEngine.kt
+++ src/main/kotlin/org/almibe/weaseltemplate/WeaselTemplateEngine.kt
@@ -4,6 +4,7 @@ import com.google.gson.JsonObject
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.util.*
 import java.util.stream.Stream
 
 enum class TokenType {
@@ -34,14 +35,19 @@ class WeaselTemplateEngine(private val classLoader: ClassLoader) {
     private val templateCache: MutableMap<String, Template> = mutableMapOf()
 
     fun processTemplate(templateName: String, data: JsonObject): String {
-        return if (templateCache.containsKey(templateName)) {
-            templateCache[templateName]?.applyTemplate(data) ?: throw RuntimeException("Error applying template $templateName")
+        val template = templateCache[templateName]
+        return if (template != null) {
+            template.applyTemplate(data)
         } else {
             val templateUrl = classLoader.getResource(templateName)
             val tokenStream = pathToStream(Paths.get(templateUrl.toURI()))
             val cache: List<Cache> = mutableListOf()
-            tokenStream.forEach {
-                //TODO fill cache list
+            val iterator = tokenStream.iterator()
+            while(iterator.hasNext()) {
+                val token = iterator.next()
+                when(token) {
+                    //TODO fill cache list
+                }
             }
             val template = Template(templateName, cache)
             templateCache[templateName] = template
@@ -70,13 +76,70 @@ class WeaselTemplateEngine(private val classLoader: ClassLoader) {
 
     }
 
+    val lookAhead: Queue<Char> = LinkedList<Char>()
+
+    private fun CharIterator.consume(): Char? {
+        //TODO handle checking look ahead
+        return if(this.hasNext()) {
+            nextChar()
+        } else {
+            null
+        }
+    }
+
+    private fun CharIterator.ignoreWS(): Char? {
+        TODO("Get next non white space character or return null")
+    }
+
+    private fun CharIterator.consume(testString: String): Boolean {
+        TODO("check if testString is the next string available")
+    }
+
     private fun pathToStream(path: Path): Stream<Token> = Files.lines(path).flatMap { line: String ->
         val tokens = mutableListOf<Token>()
-        line.forEach { char ->
-            when (char) {
-                //'{' ->
-            }
+        val iterator = line.toCharArray().iterator()
+        while(iterator.hasNext()) {
+            val char = iterator.nextChar()
+            tokens.addAll(when (char) {
+                '{' -> handleOpenCurly(iterator)
+                '}' -> handleCloseCurly(iterator)
+                else -> handleText(iterator)
+            })
         }
         tokens.stream()
+    }
+
+    private fun handleOpenCurly(iterator: CharIterator): List<Token> {
+        if (iterator.hasNext()) {
+            val char = iterator.ignoreWS()
+            when (char) {
+                '{' -> return listOf(Token("{", TokenType.ESCAPED_OPEN_CURLY))
+                'i' -> return handleIfOrReference(iterator)
+                'e' -> return handleEachOrReference(iterator)
+                else -> return handleReference(iterator)
+            }
+        } else {
+            throw RuntimeException("Error parsing template")
+        }
+    }
+
+    private fun handleCloseCurly(iterator: CharIterator): List<Token> {
+        TODO()
+    }
+
+    private fun handleText(iterator: CharIterator): List<Token> {
+        TODO()
+    }
+
+    private fun handleIfOrReference(iterator: CharIterator): List<Token> {
+        TODO()
+    }
+
+    private fun handleEachOrReference(iterator: CharIterator): List<Token> {
+        TODO()
+    }
+
+    private fun handleReference(iterator: CharIterator): List<Token> {
+        TODO()
     }
 }
