d29cd278444ff9c251f06b277a7b8379a366c91c
Alex Berry <berry3@indiana.edu>
2017-10-29 01:40:05 -0400

    moving Types related to tokens to own file + cleaning old code from WeaselTemplateEngine, #2

--- src/main/kotlin/org/almibe/weaseltemplate/WeaselTemplateEngine.kt
+++ src/main/kotlin/org/almibe/weaseltemplate/WeaselTemplateEngine.kt
@@ -20,32 +20,8 @@ import com.google.gson.JsonObject
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
-import java.util.*
 import java.util.stream.Stream
 
-enum class TokenType {
-    TEXT
-//    STRING,
-//    OPEN_CURLY,
-//    ESCAPED_OPEN_CURLY,
-//    CLOSE_CURLY,
-//    ESCAPED_CLOSE_CURLY,
-//    IF,
-//    ELSEIF,
-//    ELSE,
-//    CLOSE_IF,
-//    EACH,
-//    CLOSE_EACH,
-//    AS,
-//    REFERENCE,
-//    LOOP_NAME,
-//    KEY_NAME,
-//    VALUE_NAME,
-//    TEMPLATE_NAME,
-//    EOL
-}
-
-data class Token(val tokenType: TokenType, val value: String)
 
 class WeaselTemplateEngine(private val classLoader: ClassLoader) {
 
@@ -93,71 +69,17 @@ class WeaselTemplateEngine(private val classLoader: ClassLoader) {
 
     }
 
-    val lookAhead: Queue<Char> = LinkedList<Char>()
-
-    private fun CharIterator.consume(): Char? {
-        //TODO handle checking look ahead
-        return if(this.hasNext()) {
-            nextChar()
-        } else {
-            null
-        }
-    }
-
-    private fun CharIterator.ignoreWS(): Char? {
-        TODO("Get next non white space character or return null")
-    }
-
-    private fun CharIterator.consume(testString: String): Boolean {
-        TODO("check if testString is the next string available")
-    }
-
     private fun pathToStream(path: Path): Stream<Token> = Files.lines(path).flatMap { line: String ->
         val tokens = mutableListOf<Token>()
-        val iterator = line.toCharArray().iterator()
-        while(iterator.hasNext()) {
-            val char = iterator.nextChar()
-            tokens.addAll(when (char) {
-                '{' -> handleOpenCurly(iterator)
-                '}' -> handleCloseCurly(iterator)
-                else -> handleText(iterator)
-            })
-        }
-        tokens.stream()
-    }
-
-    private fun handleOpenCurly(iterator: CharIterator): List<Token> {
-        TODO()
-//        if (iterator.hasNext()) {
-//            val char = iterator.ignoreWS()
-//            when (char) {
-//                '{' -> return listOf(Token("{", TokenType.ESCAPED_OPEN_CURLY))
-//                'i' -> return handleIfOrReference(iterator)
-//                'e' -> return handleEachOrReference(iterator)
-//                else -> return handleReference(iterator)
-//            }
-//        } else {
-//            throw RuntimeException("Error parsing template")
+//        val iterator = line.toCharArray().iterator()
+//        while(iterator.hasNext()) {
+//            val char = iterator.nextChar()
+//            tokens.addAll(when (char) {
+//                '{' -> handleOpenCurly(iterator)
+//                '}' -> handleCloseCurly(iterator)
+//                else -> handleText(iterator)
+//            })
 //        }
-    }
-
-    private fun handleCloseCurly(iterator: CharIterator): List<Token> {
-        TODO()
-    }
-
-    private fun handleText(iterator: CharIterator): List<Token> {
-        TODO()
-    }
-
-    private fun handleIfOrReference(iterator: CharIterator): List<Token> {
-        TODO()
-    }
-
-    private fun handleEachOrReference(iterator: CharIterator): List<Token> {
-        TODO()
-    }
-
-    private fun handleReference(iterator: CharIterator): List<Token> {
-        TODO()
+        tokens.stream()
     }
 }
