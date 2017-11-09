9724e310d3495eee4a6f2f260c7d0474f90d7d33
Alex Berry <berry3@indiana.edu>
2017-11-08 23:42:26 -0500

    copying Parser so that I'll have a separate Lexer instead of trying to lex and parse in the same class/step, #10

--- src/main/kotlin/org/almibe/weaseltemplate/WeaselTemplateLexer.kt
+++ src/main/kotlin/org/almibe/weaseltemplate/WeaselTemplateLexer.kt
@@ -1,25 +1,9 @@
-/*
- * Copyright 2017 Alex Berry
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.almibe.weaseltemplate
 
 import java.util.*
 import java.util.stream.Stream
 
-class WeaselTemplateParser {
+class WeaselTemplateLexer {
     private val specialCharacter = '?'
 
     private data class IfSubTemplateBuilder(val conditionalSubTemplates: MutableList<ConditionalSubTemplate>,
@@ -37,14 +21,14 @@ class WeaselTemplateParser {
     }
 
     private data class ParserInstanceValues(
-        val consumed: StringBuilder = StringBuilder(),
-        val subTemplates: MutableList<SubTemplate> = mutableListOf(),
-        val lineNumber: Int = 0,
-        val ifTemplateBuilders: Deque<IfSubTemplateBuilder> = ArrayDeque()
+            val consumed: StringBuilder = StringBuilder(),
+            val subTemplates: MutableList<SubTemplate> = mutableListOf(),
+            val lineNumber: Int = 0,
+            val ifTemplateBuilders: Deque<IfSubTemplateBuilder> = ArrayDeque()
     )
 
     fun parse(lines: Stream<String>): List<SubTemplate> {
-        val instanceValues = WeaselTemplateParser.ParserInstanceValues()
+        val instanceValues = WeaselTemplateLexer.ParserInstanceValues()
         lines.forEach { line: String ->
             val iterator = line.toCharArray().iterator()
             while (iterator.hasNext()) {
