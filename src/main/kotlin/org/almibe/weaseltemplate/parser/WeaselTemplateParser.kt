4a1180f4aea9622c5bd1bbcbecc5a862c7f20142
Alex Berry <berry3@indiana.edu>
2017-11-12 16:22:04 -0500

    adding SubTemplateBuilder interface, adding handlers for all other Token types, #5

--- src/main/kotlin/org/almibe/weaseltemplate/parser/WeaselTemplateParser.kt
+++ src/main/kotlin/org/almibe/weaseltemplate/parser/WeaselTemplateParser.kt
@@ -20,24 +20,29 @@ import org.almibe.weaseltemplate.lexer.*
 import java.util.*
 
 class WeaselTemplateParser {
-    private data class IfSubTemplateBuilder(val conditionalSubTemplates: MutableList<ConditionalSubTemplate>,
-                                            var elseSubTemplate: ElseSubTemplate?) {
-        fun createIfTemplate(): IfSubTemplate = IfSubTemplate(conditionalSubTemplates, elseSubTemplate)
+
+    interface SubTemplateBuilder {
+        fun createSubTemplate(): SubTemplate
+    }
+
+    private data class IfSubTemplateBuilder(val conditionalSubTemplates: MutableList<ConditionalSubTemplate> = mutableListOf(),
+                                            var elseSubTemplate: ElseSubTemplate? = null): SubTemplateBuilder {
+        override fun createSubTemplate(): IfSubTemplate = IfSubTemplate(conditionalSubTemplates, elseSubTemplate)
     }
 
     private data class ConditionalSubTemplateBuilder(val conditionSelector: String,
-                                                     val content: MutableList<SubTemplate>) {
-        fun createConditionalTemplate(): ConditionalSubTemplate = ConditionalSubTemplate(conditionSelector, content)
+                                                     val content: MutableList<SubTemplate> = mutableListOf()): SubTemplateBuilder {
+        override fun createSubTemplate(): ConditionalSubTemplate = ConditionalSubTemplate(conditionSelector, content)
     }
 
-    private data class ElseSubTemplateBuilder(val content: MutableList<SubTemplate>) {
-        fun createElseTemplate(): ElseSubTemplate = ElseSubTemplate(content)
+    private data class ElseSubTemplateBuilder(val content: MutableList<SubTemplate>): SubTemplateBuilder {
+        override fun createSubTemplate(): ElseSubTemplate = ElseSubTemplate(content)
     }
 
     private data class ParserInstanceValues(
             val subTemplates: MutableList<SubTemplate> = mutableListOf(),
             val lineNumber: Int = 0,
-            val ifTemplateBuilders: Deque<IfSubTemplateBuilder> = ArrayDeque()
+            val subTemplateBuilders: Deque<SubTemplateBuilder> = ArrayDeque()
     )
 
     fun parse(tokens: List<Token>): List<SubTemplate> {
@@ -49,7 +54,11 @@ class WeaselTemplateParser {
                 is TextToken -> handleTextToken(token, instanceValues)
                 is ScalarToken -> handleScalarToken(token, instanceValues)
                 is IfToken -> handleIfToken(token, it, instanceValues)
+                is ElseIfToken -> handleElseIfToken(token, it, instanceValues)
+                is ElseToken -> handleElseToken(token, it, instanceValues)
+                is EndIfToken -> handleEndIfToken(token, it, instanceValues)
                 is EachToken -> handleEachToken(token, it, instanceValues)
+                is EndEachToken -> handleEndEachToken(token, it, instanceValues)
                 is IncludeToken -> handleIncludeToken(token, instanceValues)
                 else -> throw RuntimeException("Unexpected token $token")
             }
@@ -66,13 +75,32 @@ class WeaselTemplateParser {
     }
 
     private fun handleIfToken(token: IfToken, tokens: Iterator<Token>, instanceValues: ParserInstanceValues) {
+        val ifSubTemplateBuilder = IfSubTemplateBuilder()
+        instanceValues.subTemplateBuilders.push(ifSubTemplateBuilder)
+
+        val conditionSubTemplateBuilder = ConditionalSubTemplateBuilder(token.condition)
+        instanceValues.subTemplateBuilders.push(conditionSubTemplateBuilder)
+    }
+
+    private fun handleElseIfToken(token: ElseIfToken, tokens: Iterator<Token>, instanceValues: ParserInstanceValues) {
         TODO()
-        //instanceValues.subTemplates.add(ConditionalSubTemplate(tagTokens.component2(), listOf()))
+    }
+
+    private fun handleElseToken(token: ElseToken, tokens: Iterator<Token>, instanceValues: ParserInstanceValues) {
+        TODO()
+    }
+
+    private fun handleEndIfToken(token: EndIfToken, tokens: Iterator<Token>, instanceValues: ParserInstanceValues) {
+        TODO()
     }
 
     private fun handleEachToken(token: EachToken, tokens: Iterator<Token>, instanceValues: ParserInstanceValues) {
         TODO()
         //instanceValues.subTemplates.add(EachSubTemplate(tagTokens.component2(), tagTokens.component4()))
+    }
+
+    private fun handleEndEachToken(token: EndEachToken, tokens: Iterator<Token>, instanceValues: ParserInstanceValues) {
+        TODO()
     }
 
     private fun handleIncludeToken(token: IncludeToken, instanceValues: ParserInstanceValues) {
