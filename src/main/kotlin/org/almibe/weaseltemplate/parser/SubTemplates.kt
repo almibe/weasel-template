fa0eba6ec44a87bc904e81e7636d29bad45fbba3
Alex Berry <berry3@indiana.edu>
2017-11-13 18:56:11 -0500

    starting to add include support, #13

--- src/main/kotlin/org/almibe/weaseltemplate/parser/SubTemplates.kt
+++ src/main/kotlin/org/almibe/weaseltemplate/parser/SubTemplates.kt
@@ -101,7 +101,7 @@ data class ElseSubTemplate(val content: List<SubTemplate>) : ConditionalSubTempl
     }
 }
 
-data class EachSubTemplate(val listSelector: String, val iteratorName: String, val content: List<SubTemplate>): SubTemplate { // EachSubTemplate("users", "user")
+data class EachSubTemplate(val listSelector: String, val iteratorName: String, val content: List<SubTemplate>): SubTemplate {
     private fun selectList(data: JsonObject): JsonArray? {
         val names = listSelector.split(".")
         var current: JsonObject = data
@@ -130,9 +130,38 @@ data class EachSubTemplate(val listSelector: String, val iteratorName: String, v
     }
 }
 
-data class IncludeSubTemplate(val fileName: String, val argumentSelector: String? = null): SubTemplate { // IncludeSubTemplate("adminTemplate.wt", "user")
+data class IncludeSubTemplate(val fileName: String, val argumentSelector: String? = null): SubTemplate {
+    private fun selectArgument(data: JsonObject): JsonObject? {
+        if (argumentSelector == null) {
+            return null
+        } else {
+            val names = argumentSelector.split(".")
+            var current: JsonObject = data
+            val itr = names.iterator()
+            while (itr.hasNext()) {
+                val currentName = itr.next()
+                val element = current.get(currentName) ?: return null
+                if (itr.hasNext() && element.isJsonObject) {
+                    current = element as JsonObject
+                } else if (!itr.hasNext() && element is JsonObject) {
+                    return element
+                } else {
+                    return null
+                }
+            }
+            return null
+        }
+    }
+
     override fun apply(data: JsonObject, stringBuilder: StringBuilder) {
-        //
-        TODO()
+        val argument = selectArgument(data)
+        val result: String = if (argument != null) {
+
+            //TODO if argumentSelect != null select JsonObject as argument
+            TODO("process template with argument if it isn't null")
+        } else {
+            TODO("process template with argument")
+        }
+        stringBuilder.append(result)
     }
 }
